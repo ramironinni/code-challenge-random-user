1c2629d0c578e5be0255cd061f53faaf8aa6fc53
ramironinni <ramironinni@gmail.com>
2021-08-04 20:09:59 -0300

    Delete preset files. Update file tree. Add navbar component

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="navbar-icon">
+                <span className="material-icons">home</span>
+            </div>
+            <h1 className="navbar-title">Meet Our Staff!</h1>
+        </nav>
+    );
+};
+
+export default Navbar;
