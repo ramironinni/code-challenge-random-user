4a20c3da920b705e950c8afdb3dbd0a37988496b
ramironinni <ramironinni@gmail.com>
2021-08-05 19:53:54 -0300

    Add UserTile iteration that allows to set max Users per page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,40 +8,40 @@ const Home = () => {
         'https://randomuser.me/api/?inc=name,location,email,login,picture&results=50'
     );
 
+    let maxUsers = 2;
+
+    const handleClick = (e) => {
+        console.log(e.target);
+    };
+
     return (
         <div className="home">
             {data && (
                 <div className="wrapper">
                     <div className="user-tiles-container">
-                        <UserTile
-                            picture={data.results[0].picture.medium}
-                            fullName={
-                                data.results[0].name.first +
-                                ' ' +
-                                data.results[0].name.last
-                            }
-                            city={data.results[0].location.city}
-                            country={data.results[0].location.country}
-                            user={data.results[0]}
-                        />
-                        <UserTile
-                            picture={data.results[1].picture.medium}
-                            fullName={
-                                data.results[1].name.first +
-                                ' ' +
-                                data.results[1].name.last
-                            }
-                            city={data.results[1].location.city}
-                            country={data.results[1].location.country}
-                            user={data.results[1]}
-                        />
+                        {data.results.map((user, index) =>
+                            index < maxUsers ? (
+                                <UserTile
+                                    picture={user.picture.medium}
+                                    fullName={
+                                        user.name.first + ' ' + user.name.last
+                                    }
+                                    city={user.location.city}
+                                    country={user.location.country}
+                                    user={user}
+                                    key={index}
+                                />
+                            ) : (
+                                ''
+                            )
+                        )}
                     </div>
                 </div>
             )}
             {/* {data && console.log(data)} */}
             {isPending && <div>Loading ...</div>}
             {error && <div>{error}</div>}
-            <div className="navigation-btns">
+            <div className="navigation-btns" onClick={handleClick}>
                 <NavigationBtn
                     icon="navigate_before"
                     type="navigation-btn-before"
