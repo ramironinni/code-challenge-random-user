89ce2944855287073b5ab5b06e167e0d71f5d1db
ramironinni <ramironinni@gmail.com>
2021-08-06 19:07:39 -0300

    Fix env to deploy in gh pages

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,7 +7,7 @@ import NotFound from '../NotFound/NotFound';
 
 function App() {
     return (
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <div className="App">
                 <Navbar />
                 <Switch>
